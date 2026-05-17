f90871777ff3eafd0402ebfcd62bf5ff09fe94eb
felix-antonio-sl <felixsanhueza@me.com>
2026-05-17 04:54:44 +0200

    refactor(app): introduce puerto de arbol opd

---
+++ app/src/app/ports/opdTreePort.ts
@@ -0,0 +1,20 @@
+import type { OpmStore } from "../../store";
+
+export interface OpdTreePort {
+  vistaMapaActiva: OpmStore["vistaMapaActiva"];
+  modoOrdenArbol: OpmStore["modoOrdenArbol"];
+  fijarModoOrdenArbol: OpmStore["fijarModoOrdenArbol"];
+  seleccionarEntidad: OpmStore["seleccionarEntidad"];
+  eliminarOpdDesdeArbol: OpmStore["eliminarOpdDesdeArbol"];
+  moverHermano: OpmStore["moverHermano"];
+  moverOpdEnGestion: OpmStore["moverOpdEnGestion"];
+  renombrarOpdDesdeArbol: OpmStore["renombrarOpdDesdeArbol"];
+  nombresArbolVisibles: OpmStore["nombresArbolVisibles"];
+  toggleNombresArbolVisibles: OpmStore["toggleNombresArbolVisibles"];
+  navegarOpdArriba: OpmStore["navegarOpdArriba"];
+  navegarOpdAbajo: OpmStore["navegarOpdAbajo"];
+  navegarOpdIzquierda: OpmStore["navegarOpdIzquierda"];
+  navegarOpdDerecha: OpmStore["navegarOpdDerecha"];
+  abrirVistaMapa: OpmStore["abrirVistaMapa"];
+  abrirGestionArbol: OpmStore["abrirGestionArbol"];
+}
---
+++ app/src/app/ports/zustandOpdTreePort.ts
@@ -0,0 +1,40 @@
+import { useOpmStore } from "../../store";
+import type { OpdTreePort } from "./opdTreePort";
+
+export function useZustandOpdTreePort(): OpdTreePort {
+  const vistaMapaActiva = useOpmStore((s) => s.vistaMapaActiva);
+  const modoOrdenArbol = useOpmStore((s) => s.modoOrdenArbol);
+  const fijarModoOrdenArbol = useOpmStore((s) => s.fijarModoOrdenArbol);
+  const seleccionarEntidad = useOpmStore((s) => s.seleccionarEntidad);
+  const eliminarOpdDesdeArbol = useOpmStore((s) => s.eliminarOpdDesdeArbol);
+  const moverHermano = useOpmStore((s) => s.moverHermano);
+  const moverOpdEnGestion = useOpmStore((s) => s.moverOpdEnGestion);
+  const renombrarOpdDesdeArbol = useOpmStore((s) => s.renombrarOpdDesdeArbol);
+  const nombresArbolVisibles = useOpmStore((s) => s.nombresArbolVisibles);
+  const toggleNombresArbolVisibles = useOpmStore((s) => s.toggleNombresArbolVisibles);
+  const navegarOpdArriba = useOpmStore((s) => s.navegarOpdArriba);
+  const navegarOpdAbajo = useOpmStore((s) => s.navegarOpdAbajo);
+  const navegarOpdIzquierda = useOpmStore((s) => s.navegarOpdIzquierda);
+  const navegarOpdDerecha = useOpmStore((s) => s.navegarOpdDerecha);
+  const abrirVistaMapa = useOpmStore((s) => s.abrirVistaMapa);
+  const abrirGestionArbol = useOpmStore((s) => s.abrirGestionArbol);
+
+  return {
+    vistaMapaActiva,
+    modoOrdenArbol,
+    fijarModoOrdenArbol,
+    seleccionarEntidad,
+    eliminarOpdDesdeArbol,
+    moverHermano,
+    moverOpdEnGestion,
+    renombrarOpdDesdeArbol,
+    nombresArbolVisibles,
+    toggleNombresArbolVisibles,
+    navegarOpdArriba,
+    navegarOpdAbajo,
+    navegarOpdIzquierda,
+    navegarOpdDerecha,
+    abrirVistaMapa,
+    abrirGestionArbol,
+  };
+}
