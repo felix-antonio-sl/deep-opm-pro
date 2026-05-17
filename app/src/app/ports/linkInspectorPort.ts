0b40679f8a9ebb373854db4b9e09eec85485488a
felix-antonio-sl <felixsanhueza@me.com>
2026-05-17 05:07:50 +0200

    refactor(app): introduce puerto de inspector enlaces

---
+++ app/src/app/ports/linkInspectorPort.ts
@@ -0,0 +1,70 @@
+import type { OpmStore } from "../../store";
+
+export interface LinkInspectorSessionPort {
+  modelo: OpmStore["modelo"];
+  opdActivoId: OpmStore["opdActivoId"];
+  tabActivo: OpmStore["tabInspectorEnlaceActivo"];
+  cambiarTab: OpmStore["cambiarTabInspectorEnlace"];
+  cambiarOpdActivo: OpmStore["cambiarOpdActivo"];
+  abrirInspectorEnlaceDesdeOpl: OpmStore["abrirInspectorEnlaceDesdeOpl"];
+}
+
+export interface SelectedLinkPropertiesPort {
+  ajustarMultiplicidad: OpmStore["ajustarMultiplicidadSeleccionada"];
+  alternarOperadorAbanico: OpmStore["alternarOperadorAbanicoSeleccionado"];
+  quitarRamaDeAbanico: OpmStore["quitarRamaDeAbanicoSeleccionado"];
+  disolverAbanico: OpmStore["disolverAbanicoSeleccionado"];
+  aplicarModificador: OpmStore["aplicarModificadorEnlaceSeleccionado"];
+  aplicarSubtipoModificador: OpmStore["aplicarSubtipoModificadorEnlaceSeleccionado"];
+  quitarModificador: OpmStore["quitarModificadorEnlaceSeleccionado"];
+  definirProbabilidadEvento: OpmStore["definirProbabilidadEventoSeleccionada"];
+  definirDemoraInvocacion: OpmStore["definirDemoraInvocacionSeleccionada"];
+  definirBackwardTag: OpmStore["definirBackwardTagSeleccionado"];
+  definirRequisitosEnlace: OpmStore["definirRequisitosEnlaceSeleccionado"];
+  definirTasaEnlace: OpmStore["definirTasaEnlaceSeleccionada"];
+  definirTiempoExcepcionEnlace: OpmStore["definirTiempoExcepcionEnlaceSeleccionado"];
+  renombrarEtiquetaEnlace: OpmStore["renombrarEtiquetaEnlaceSeleccionado"];
+  definirRutaEtiqueta: OpmStore["definirRutaEtiquetaSeleccionada"];
+}
+
+export interface SelectedLinkEndpointsPort {
+  apuntarExtremo: OpmStore["apuntarExtremoEnlaceSeleccionado"];
+  moverPuerto: OpmStore["moverPuertoEnlaceSeleccionado"];
+  reanclarEnlaceExternoDerivado: OpmStore["reanclarEnlaceExternoDerivado"];
+  volverEnlaceExternoDerivadoAAutomatico: OpmStore["volverEnlaceExternoDerivadoAAutomatico"];
+  splitEffect: OpmStore["splitEffectSeleccionado"];
+}
+
+export interface StructuralLinkGroupPort {
+  cambiarTipoGrupoEstructural: OpmStore["cambiarTipoGrupoEstructuralSeleccionado"];
+  fijarOrdenGrupoEstructural: OpmStore["fijarOrdenGrupoEstructuralSeleccionado"];
+  actualizarAnclajesSimboloEstructural: OpmStore["actualizarAnclajesSimboloEstructural"];
+  resetearAnclajesSimboloEstructural: OpmStore["resetearAnclajesSimboloEstructural"];
+  separarGrupoEstructural: OpmStore["separarGrupoEstructuralSeleccionado"];
+  volverGrupoEstructuralAutomatico: OpmStore["volverGrupoEstructuralAutomaticoSeleccionado"];
+  traerRelacionesEstructuralesFaltantes: OpmStore["traerRelacionesEstructuralesFaltantesSeleccionadas"];
+  plegarGrupoEstructural: OpmStore["plegarGrupoEstructuralSeleccionado"];
+  plegarCompletoGrupoEstructural: OpmStore["plegarCompletoGrupoEstructuralSeleccionado"];
+}
+
+export interface LinkStylePort {
+  aplicarEstiloEnlaceAccion: OpmStore["aplicarEstiloEnlaceAccion"];
+  resetEstiloEnlaceAccion: OpmStore["resetEstiloEnlaceAccion"];
+  copiarEstiloAlPortapapeles: OpmStore["copiarEstiloEnlaceAlPortapapeles"];
+  pegarEstiloDesdePortapapeles: OpmStore["pegarEstiloEnlaceDesdePortapapeles"];
+  enlaceEstiloPortapapeles: OpmStore["enlaceEstiloPortapapeles"];
+  seleccionados: OpmStore["seleccionados"];
+  aplicarEstiloASeleccion: OpmStore["aplicarEstiloASeleccion"];
+}
+
+export interface LinkDeletionPort {
+  eliminar: OpmStore["eliminarSeleccion"];
+}
+
+export interface LinkInspectorPort extends
+  LinkInspectorSessionPort,
+  SelectedLinkPropertiesPort,
+  SelectedLinkEndpointsPort,
+  StructuralLinkGroupPort,
+  LinkStylePort,
+  LinkDeletionPort {}
